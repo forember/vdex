c2d6e57956d132eed3941ee7365dbb06ce640590
Chris McKinney <ntd5@tachibanatech.com>
2018-11-19 09:27:05 -0500

    Back on track

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,7 @@
 extern crate enum_repr;
-extern crate libc;
-
-use libc::*;
 
 use enum_repr::EnumRepr;
 
-
-#[EnumRepr(type = "c_int")]
-#[derive(Debug, PartialEq)]
-pub enum Test {
-    A,
-    B,
-    C = 5,
-    D,
-}
-
-fn main() {
-    assert_eq!(Test::B.repr(), 1);
-    assert_eq!(Test::from_repr(6), Some(Test::D));
-    assert!(Test::from_repr(2).is_none());
-}
-
-/*
 #[EnumRepr(type = "u8")]
 #[derive(Debug)]
 pub enum Abilities {
@@ -34,4 +14,3 @@ fn main() {
     let ability = Abilities::from_repr(3);
     println!("ability: {:?}", ability);
 }
-*/
