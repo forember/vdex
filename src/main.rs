6523edca843a9ad833af9498a2eb9c0eb5421674
Chris McKinney <ntd5@tachibanatech.com>
2018-11-19 07:50:07 -0500

    Fixed enum-repr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate enum_repr;
+
+use enum_repr::EnumRepr;
+
+#[EnumRepr(type = "u8")]
+#[derive(Debug)]
+pub enum Abilities {
+    Stench = 1,
+    Drizzle,
+    SpeedBoost,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ability = Abilities::from_repr(3);
+    println!("ability: {:?}", ability);
 }
